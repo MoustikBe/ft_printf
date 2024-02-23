a6916799388eaf363ede1329529483e91a06dd1d
Max <81183216+MoustikBe@users.noreply.github.com>
2024-02-23 19:34:15 +0100

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,13 @@
+#ifndef FT_PRINTF
+# define FT_PRINTF
+// # -- Library -- # //
+# include "stdio.h"
+# include <stdarg.h>
+# include <stdio.h>
+# include <stddef.h>
+# include <unistd.h>
+// # -- Library -- # //
+// # -- Function -- # //
+int			ft_printf(const char *str, ...);
+// # -- Function -- # //
+#endif
